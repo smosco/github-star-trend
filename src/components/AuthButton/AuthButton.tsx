97470039bd74756ba3b2ec05f261a13e2939d0d6
smosco <go1ruf2tk3@gmail.com>
2025-01-20 15:45:58 +0900

    style: 로그인 버튼 스타일링

--- src/components/AuthButton/AuthButton.tsx
+++ src/components/AuthButton/AuthButton.tsx
@@ -1,20 +1,22 @@
 import { useSession, signIn, signOut } from 'next-auth/react';
+import * as styles from './AuthButton.css';
 
 export default function AuthButton() {
   const { data: session } = useSession();
 
   if (session) {
     return (
-      <>
-        Signed in as {session.user?.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
+      <div className={styles.container}>
+        <span className={styles.userName}>{session.user?.name}</span>
+        <button className={styles.signOutButton} onClick={() => signOut()}>
+          Sign out
+        </button>
+      </div>
     );
   }
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
+    <button className={styles.signInButton} onClick={() => signIn()}>
+      Sign in
+    </button>
   );
 }
