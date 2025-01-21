732036f0b6b46254e4aa3caa22a47a9fad1eea66
smosco <go1ruf2tk3@gmail.com>
2025-01-21 13:22:44 +0900

    feat: graphql request로 직접 요청하던 개발자별 스타한 레포를 Next의 API Route를 사용하도록 변경

--- src/components/StarredReposByDeveloper/StarredReposByDeveloper.tsx
+++ src/components/StarredReposByDeveloper/StarredReposByDeveloper.tsx
@@ -1,33 +1,8 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { githubClient } from '@/utils/githubClient';
-import { STARRED_REPOS_BY_DEVELOPER } from '@/graphql/queries/starredReposByDeveloper.ts';
 import * as styles from './StarredReposByDeveloper.css';
 import DeveloperStars from '@/components/DeveloperStars/DeveloperStars';
-
-interface GetStarredReposResponse {
-  user: {
-    id: string;
-    name: string;
-    login: string;
-    avatarUrl: string;
-    bio: string;
-    starredRepositories: {
-      edges: {
-        starredAt: string;
-        node: {
-          name: string;
-          owner: { login: string };
-          url: string;
-          description: string;
-          stargazerCount: number;
-          primaryLanguage: { name: string } | null;
-        };
-      }[];
-    };
-  };
-}
 
 interface Repo {
   name: string;
@@ -49,69 +24,40 @@ interface Developer {
 }
 
 const StarredReposByDeveloper = () => {
-  const developers = ['terkelg', 'lukeed', 'developit'];
-
-  const [developerRepos, setDeveloperRepos] = useState<Developer[]>([]);
+  const [developersData, setDevelopersData] = useState<Developer[]>([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const fetchStarredRepos = async () => {
+    const fetchDevelopersData = async () => {
       setLoading(true);
-
       try {
-        const fetchPromises = developers.map(async (developer) => {
-          try {
-            const variables = { username: developer, first: 10 };
-            const data: GetStarredReposResponse = await githubClient.request(
-              STARRED_REPOS_BY_DEVELOPER,
-              variables
-            );
-
-            const repos = data.user.starredRepositories.edges.map((repo) => ({
-              name: repo.node.name,
-              owner: repo.node.owner.login,
-              url: repo.node.url,
-              description: repo.node.description,
-              stars: repo.node.stargazerCount,
-              language: repo.node.primaryLanguage?.name || 'Unknown',
-              starredAt: repo.starredAt,
-            }));
-
-            return {
-              id: data.user.id,
-              name: data.user.name,
-              username: data.user.login,
-              avatar: data.user.avatarUrl,
-              bio: data.user.bio,
-              starredRepos: repos,
-            };
-          } catch (error: any) {
-            console.error(`Failed to fetch data for ${developer}:`, error);
-            return null;
-          }
+        const response = await fetch('/api/github-starred-repos', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            developers: ['terkelg', 'lukeed', 'developit'], // 원하는 개발자 배열
+          }),
         });
 
-        const responses = await Promise.all(fetchPromises);
-        const filteredResponses = responses.filter(
-          (response): response is Developer => response !== null
-        );
+        if (!response.ok) {
+          throw new Error('Failed to fetch developers data');
+        }
 
-        setDeveloperRepos(filteredResponses);
+        const data = await response.json();
+        setDevelopersData(data.developers);
+      } catch (error) {
+        setError('Failed to fetch starred repositories by developers');
       } finally {
         setLoading(false);
       }
     };
 
-    fetchStarredRepos();
+    fetchDevelopersData();
   }, []);
 
-  if (loading) {
-    return (
-      <div className={styles.loaderContainer}>
-        <div className={styles.spinner}></div>
-      </div>
-    );
-  }
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
 
   return (
     <div className={styles.contentContainer}>
@@ -119,7 +65,7 @@ const StarredReposByDeveloper = () => {
         Starred Repositories By Developer
         <span className={styles.subtitle}>(Last 1 month)</span>
       </h2>
-      {developerRepos.map((developer) => (
+      {developersData.map((developer) => (
         <DeveloperStars key={developer.username} developer={developer} />
       ))}
     </div>
